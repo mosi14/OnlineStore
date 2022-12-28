565569e341bc314cddb589c3e3d8d7e603ff86b8
mosi14 <m.asadollahy@hotmail.com>
2022-12-28 18:37:20 +0100

    implement order

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
 
 dotenv.config();
+
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
   console.log("connected to the db")
 }).catch((err)=>{
@@ -20,6 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
