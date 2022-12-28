8e4e347d9938da31df66480158ab8d9cd1dccd74
mosi14 <m.asadollahy@hotmail.com>
2022-12-28 14:32:25 +0100

    add backend of signin

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
@@ -13,8 +14,17 @@ mongoose.connect(process.env.MONGODB_URL).then(()=>{
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
 
 
 const port = process.env.PORT || 5000;
